54a77928a32b427e4a0871ab9496c99932657a0e
kenta872 <nakaikenta@outlook.com>
2021-03-01 18:16:54 +0900

    とるぐメニュー修正

--- climarks/js/script.js
+++ climarks/js/script.js
@@ -18,22 +18,62 @@ $(function(){
   });
 });
 $(function(){
-  var w = $(window).width();
-  var limit = 980;
-  var serviceCheck=false;
+  var serviceCheck = false;
   $("#serviceOpen").click(function(){
-    if(w<=limit) {
-      $("#service").addClass("serviceOP");
-      if(serviceCheck==false) {
-        serviceCheck=true;
-        $(".serviceOP").css({"display":"block"});
-      }else {
-        serviceCheck=false;
-        $(".serviceOP").css({"display":"none"});
-      }
-    }else {
-      $("#service").removeClass("serviceOP");
+    if (serviceCheck == false) {
+      serviceCheck=true;
+      $("#serviceList").css({"display":"block"});
+    } else {
+      serviceCheck=false;
+      $("#serviceList").css({"display":"none"});
     }
     return false;
   });
+});
+$(function(){
+  var serviceCheck = false;
+  $("#creativeOpen").click(function(){
+    if (serviceCheck == false) {
+      serviceCheck=true;
+      $("#creativeList").css({"display":"block"});
+    } else {
+      serviceCheck=false;
+      $("#creativeList").css({"display":"none"});
+    }
+    return false;
+  });
+});
+$(function(){
+  var serviceCheck = false;
+  $("#companyOpen").click(function(){
+    if (serviceCheck == false) {
+      serviceCheck=true;
+      $("#companyList").css({"display":"block"});
+    } else {
+      serviceCheck=false;
+      $("#companyList").css({"display":"none"});
+    }
+    return false;
+  });
+});
+$(function(){
+  var serviceCheck = false;
+  $("#reqruitOpen").click(function(){
+    if (serviceCheck == false) {
+      serviceCheck=true;
+      $("#reqruitList").css({"display":"block"});
+    } else {
+      serviceCheck=false;
+      $("#reqruitList").css({"display":"none"});
+    }
+    return false;
+  });
+});
+
+$(window).resize(function(){
+  var w = $(window).width();
+  var limit = 980;
+  if (w>=limit) {
+    $("#serviceList").css({"display":""});
+  }
 });
